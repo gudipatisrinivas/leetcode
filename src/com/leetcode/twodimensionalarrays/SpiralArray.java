8f04c0c3fb0600b764e56af100d299388be6be90
srinivasgudipati <srinivasgudipati@macbook-pro>
2019-07-23 13:58:25 -0400

    Intial Commit

---
+++ src/com/leetcode/twodimensionalarrays/SpiralArray.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ * @Author srinivas gudipati
+ * @Since  201912:00:22 PM
+ */ 
+package com.leetcode.twodimensionalarrays;
+
+/**
+ * @author srinivas gudipati
+ *
+ */
+public class SpiralArray {
+	
+	private static int[][] spiralArray(int n){
+		 
+        int[][] spiral = new int[n][n];
+         
+        int value = 1;
+         
+        int minCol = 0;
+         
+        int maxCol = n-1;
+         
+        int minRow = 0;
+         
+        int maxRow = n-1;
+         
+       while(value<=n*n) {
+    	   for(int i=minCol;i<=maxCol;i++) {
+    		   spiral[minRow][i]=value;
+    		   value++;
+    	   }
+    	   for(int i=minRow+1;i<=maxRow;i++) {
+    		   spiral[i][maxCol]=value;
+    		   value++;
+    	   }
+    	   for(int i=maxCol-1;i>=minCol;i--) {
+    		   spiral[maxRow][i]=value;
+    		   value++;
+    	   }
+    	   for(int i=maxRow-1;i>=minRow+1;i--) {
+    		   spiral[i][minCol]=value;
+    		   value++;
+    	   }
+    	   
+    	   minCol++;
+    	   minRow++;
+    	   maxCol--;
+    	   maxRow--;
+    	   
+       }
+         
+        for (int i = 0; i < spiral.length; i++)
+        {
+            for (int j = 0; j < spiral.length; j++)
+            {
+                System.out.print(spiral[i][j]+ "\t");
+            }
+             
+            System.out.println();
+        }
+		return spiral;
+    }
+	
+	public static void main(String[] s) {
+		spiralArray(5);
+		
+	}
+
+}
